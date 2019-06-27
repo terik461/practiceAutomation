c2b25c961e48ce6bbf5e603249d859a2376d5506
unknown <terik46@yahoo.com>
2019-06-26 20:34:54 -0700

    Done

---
+++ tests/yoodalize.js
@@ -0,0 +1,18 @@
+1
+Open alpha.yoodlize.com
+
+
+
+
+
+2
+Repeat the following steps for the "See all" links near each category heading AND the "Browse Categories" tiles at the bottom of the page.
+
+
+
+
+
+3
+Open the link.
+
+You are on the search screen with the appropriate category filter applied.
